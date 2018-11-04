871b05b35c3ee53aa3e8d10e9135f590c433392f
Sulabasana <piotr.frydryszak@gmail.com>
2018-11-04 14:06:04 +0100

    Tic Tac Toe

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+/*wnętrze kwadratów*/
+class Square extends React.Component {
+  constructor(props){
+    super(props);//always when you use constructor
+    this.state = {
+      value: null,
+    }
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}/*alert('click')} /<button className="square" onClick= {function() {console.log('click'); }}*/> 
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+/*Rysuje tablice do gry i daje napis */
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value ={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+/* */
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+serviceWorker.unregister();
